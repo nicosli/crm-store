011f71ada077e7478a821b21659bce478755738a
nicosli <nicosli@gmail.com>
2019-03-18 10:09:19 -0600

    Se cambia por ruta

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -19,7 +19,6 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        'https://api.bitbucket.org/*',
-        'https://bitbucket.org/*'
+        '/project/update'
     ];
 }
