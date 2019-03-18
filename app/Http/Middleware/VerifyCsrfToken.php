a3793e49bf23376ef5171699d388d2ce68f52b9f
nicosli <nicosli@gmail.com>
2019-03-18 10:03:57 -0600

    Se excluye bitbucket a VerifyCsrToken

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -19,6 +19,7 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        //
+        'https://api.bitbucket.org/*',
+        'https://bitbucket.org/+'
     ];
 }
